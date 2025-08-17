23d17c6c0903d79387eb3109ef9b9eee8187ad79
Ahmed Atef Elnadi <ahmedatefelnadiCOURSESONLINE@GMAIL.COM>
2025-08-17 13:16:14 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2025 Ahmed Atef Elnadi
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+
 const express = require('express');
 const redis = require('redis');
 const { Pool } = require('pg');
