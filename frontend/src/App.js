d281fc515c0cbebc04416cb3f8c24df559641532
Ahmed Atef Elnadi <ahmedatefelnadiCOURSESONLINE@GMAIL.COM>
2025-08-17 13:15:45 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2025 Ahmed Atef Elnadi
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+
 import React, { useEffect, useState } from 'react';
 
 function App() {
